52cbefb7bca5360152063affb1690c8fce574235
SmallGuy69 <grim25475@gmail.com>
2026-01-16 19:54:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,6 @@
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+window.global = window;
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
